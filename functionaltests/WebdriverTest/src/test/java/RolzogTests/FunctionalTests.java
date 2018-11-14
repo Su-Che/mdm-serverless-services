d79d46a3c59e44d063e05a4511849ce1a724dc0c
Megha Havaragi <meghahavaragi@gmail.com>
2018-11-14 19:49:31 +0530

    add functional test

--- functionaltests/WebdriverTest/src/test/java/RolzogTests/FunctionalTests.java
+++ functionaltests/WebdriverTest/src/test/java/RolzogTests/FunctionalTests.java
@@ -2,17 +2,17 @@ package RolzogTests;
 
 
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
 
 public class FunctionalTests {
 
@@ -23,24 +23,19 @@ public class FunctionalTests {
 
         driver = new ChromeDriver();
         String currentUsersWorkingDir = System.getProperty("user.dir");
-    	
+
     	System.setProperty("webdriver.chrome.driver",currentUsersWorkingDir+"/chromedriver");
-    	
+
     }
-    
-    @BeforeTest
-    public void beforeTest() {	
-	    
-	}
-    
+
 	@AfterTest
 	public void afterTest() {
-		//driver.quit();			
+		driver.quit();
 	}
-    
+
     @Test(priority=1)
 
-    public void ValidateRegistrationPageAndLandingOnOktaPage() {
+    public void C() {
 
 
         String baseUrl = "https://api.mdmdev.thoughtworks.net/rolzog/?serial=C02L70VFF5YW";
@@ -48,8 +43,10 @@ public class FunctionalTests {
         driver.get(baseUrl);
         String actualTitle = driver.getTitle();
 
+        SoftAssert softAssertion= new SoftAssert();
+
         //Validate title
-        Assert.assertEquals(actualTitle, expectedTitle);
+        softAssertion.assertEquals(actualTitle, expectedTitle);
         //Validate redirection to Okta page
         driver.findElement(By.linkText("Register")).click();
 
@@ -57,33 +54,33 @@ public class FunctionalTests {
         //ThoughtWorks UD - Sign In
         String actualOktaTitle = driver.getTitle();
 
-        Assert.assertEquals(actualOktaTitle, actualOktaTitle);
-        
+        softAssertion.assertEquals(actualOktaTitle, actualOktaTitle);
+
         WebElement userName =  ((ChromeDriver) driver).findElement(By.id("okta-signin-username"));
         WebElement password =  ((ChromeDriver) driver).findElement(By.id("okta-signin-password"));
 
         String usernameOkta = System.getenv("okta_username");
         String passwordOkta = System.getenv("okta_password");
-        
+
         //  WebElement password = ((ChromeDriver) driver).findElementsById("okta-signin-password");
         userName.sendKeys(usernameOkta);
         password.sendKeys(passwordOkta);
-
         password.submit();
 
-        //Validate the landing page : should be the ack page TODO
-
+        //Validate the landing page : should be the ack page z
         String ackPageUrl = driver.getCurrentUrl();
+        System.out.print(ackPageUrl);
         //Assert.assertEquals(ackPageUrl, "https://api.mdmdev.thoughtworks.net/rolzog/register?live=1&serial=C02L70VFF5YW" );
+        String successMessage = "Please reboot to complete the enrolment process.";
+        String bodyText = driver.findElement(By.tagName("body")).getText();
+        Assert.assertTrue(bodyText.contains(successMessage));
 
     }
-    
-    @Test(priority=2)
-    public void ValidateAssetStatusinServiceNow() {/*
+
+    @Test(enabled = false)
+    public void ValidateAssetStatusinServiceNow() {
     	 // Validate the status of the resource in ServiceNow:-
         String serviceNowUrl = "https://thoughtworksdev.service-now.com/navpage.do";
-        String adminUsername = "ATFAdmin";
-        String adminPassword = "Password@123";
 
         driver.get(serviceNowUrl);
         WebElement userNameSN =  ((ChromeDriver) driver).findElement(By.id("user_name"));
@@ -91,7 +88,7 @@ public class FunctionalTests {
 
         //  WebElement password = ((ChromeDriver) driver).findElementsById("okta-signin-password");
         userNameSN.sendKeys("ATFAdmin");
-        passwordSN.sendKeys("Password@123");
+        passwordSN.sendKeys("");
         passwordSN.submit();
 
         //Click on Impersonate user link:
@@ -101,11 +98,7 @@ public class FunctionalTests {
 
         String xpathImpersonate = "*[@class='dropdown-menu’]//*[text()=‘Impersonate User’]";
         ((ChromeDriver) driver).findElementByXPath(xpathImpersonate).click();
-
-
-
         driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
-       
-    */}
-}
 
+    }
+}
